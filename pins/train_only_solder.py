ccc5d3d9e90727dbd33430a26ddb416277bceeaa
trevol <iamvovan@mail.ru>
2019-09-11 12:59:00 +0300

    prepare masks

---
+++ pins/train_only_solder.py
@@ -0,0 +1,30 @@
+from model import *
+from data import *
+
+
+def main():
+    data_gen_args = dict(rotation_range=0.2,
+                         width_shift_range=0.05,
+                         height_shift_range=0.05,
+                         shear_range=0.05,
+                         zoom_range=0.05,
+                         horizontal_flip=True,
+                         fill_mode='nearest')
+    myGene = trainGenerator(2, 'data', 'image', 'solder_only_masks', data_gen_args, image_color_mode="rgb",
+                            target_size=(512, 512),
+                            save_to_dir=None)
+    model = unet(input_size=(512, 512, 3))
+    model_checkpoint = ModelCheckpoint('unet_pins_w_solder.hdf5', monitor='loss', verbose=1, save_best_only=True)
+    model.fit_generator(myGene, steps_per_epoch=2000, epochs=5, callbacks=[model_checkpoint])
+
+    # imgs_train,imgs_mask_train = geneTrainNpy("data/membrane/train/aug/","data/membrane/train/aug/")
+    # model.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=10, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+
+    # testGene = testGenerator("data/membrane/test")
+    # model = unet()
+    # model.load_weights("unet_pins.hdf5")
+    # results = model.predict_generator(testGene, 30, verbose=1)
+    # saveResult("data/membrane/test", results)
+
+
+main()

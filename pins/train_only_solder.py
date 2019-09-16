e938b92c2a1828195c6bd7bffd62849477f6de64
trevol <iamvovan@mail.ru>
2019-09-16 09:16:53 +0300

    start training on rgb solder dataset

--- pins/train_only_solder.py
+++ pins/train_only_solder.py
@@ -15,20 +15,21 @@ def main():
                          zoom_range=0.05,
                          horizontal_flip=True,
                          fill_mode='nearest')
+    target_size = (512, 512)
     myGene = trainGenerator(arg.batch_size, 'data', 'image', 'solder_only_masks', data_gen_args, image_color_mode="rgb",
-                            target_size=(512, 512),
+                            target_size=target_size,
                             save_to_dir=None)
 
-    model = unet(input_size=(512, 512, 3))
+    model = unet(input_size=target_size + (3,))
     model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
     model.load_weights('../checkpoints/membrane/color/unet_membrane_17_0.042_0.982.hdf5')
 
     checkpointsDir = 'checkpoints/rgb_solder'
     os.makedirs(checkpointsDir, exist_ok=True)
-    model_checkpoint = ModelCheckpoint(f'{checkpointsDir}/unet_solder_{{epoch}}_{{loss:.3f}}_{{acc:.3f}}.hdf5',
-                                       monitor='loss',
-                                       verbose=1, save_best_only=True)
+    chptPath = f'{checkpointsDir}/unet_solder{target_size[0]}x{target_size[1]}_{{epoch}}_{{loss:.3f}}_{{acc:.3f}}.hdf5'
+    model_checkpoint = ModelCheckpoint(chptPath, monitor='loss', verbose=1, save_best_only=True)
 
     model.fit_generator(myGene, steps_per_epoch=2000, epochs=20, callbacks=[model_checkpoint])
+
 
 main()

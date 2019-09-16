2cd3dbf9ef48578c59b529e39734d77230ef6eca
trevol <iamvovan@mail.ru>
2019-09-16 09:11:53 +0300

    start training on rgb solder dataset

--- pins/train_only_solder.sh
+++ pins/train_only_solder.sh
@@ -2,9 +2,7 @@
 
 ROOT="$(pwd)/.."
 
-# activate venv
-#source $ROOT/venv/bin/activate
+source $ROOT/venv/bin/activate
 export PYTHONPATH=$PYTHONPATH:$ROOT
 
-# sudo mount /dev/sdb2 /mnt/HDD
 python train_only_solder.py "$*"
